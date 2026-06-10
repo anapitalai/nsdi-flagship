235dc1c41f741edd435b3671280d42fa336845c9
Alois <anapitalai@gmail.com>
2026-06-10 13:57:17 +1000

    feat: apply favicon and feature-tracker updates

---
+++ context/features/add-favicon.md
@@ -0,0 +1,4 @@
+## FAVICON
+
+* Generate and add a favicon, making it PNG colors
+* Create something that blends PNG and spatial data
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export const metadata: Metadata = {
   description: "NSDI",
   icons: {
     icon: [
+      { url: "/favicon.png", type: "image/png", sizes: "256x256" },
       { url: "/favicon.svg", type: "image/svg+xml", sizes: "256x256" },
       { url: "/favicon.ico", type: "image/x-icon" },
     ],
